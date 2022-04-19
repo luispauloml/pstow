c6af0d48688bd555c4da7cd11a44bb9999211fcb
luispauloml <luispauloml@gmail.com>
2022-04-19 20:01:22 +0900

    Add worker function

--- winstow.psm1
+++ winstow.psm1
@@ -30,4 +30,40 @@ function winstow {
 
     # Check $TargetDir; an error will be thrown if it does not exists
     $Target = Resolve-Path -Path $Target
+
+    # Recursively create symbolic links
+    function worker($Item, $Dir){
+	switch($item.GetType().Name) {
+	    {$_ -eq "DirectoryInfo"}
+	    {$isDir = $true}
+
+	    {$_ -eq "FileInfo"}
+	    {$isDir = $false}
+
+	    default
+	    {Write-Error "something that is neither a file nor a directory was found : '$($item.FullName)' of type '$($item.GetType().Name)'"}
+	}
+
+	$LinkPath = Join-Path $Dir $Item.Name
+	$FileExists = Test-Path -Path $LinkPath
+	if (!$FileExists) {
+	    New-Item -ItemType SymbolicLink `
+	      -Path $LinkPath -Target $Item.FullName
+	    Write-Verbose "target does not exists; symbolic link created for '$($item.Name)'"
+	    return
+	} else {
+	    Write-Error "$($item.Name) already exists at $linkPath" `
+	      -Category WriteError
+	}
+
+	if ($isDir) {
+	    Write-Verbose "$item is a directory"
+	    $Contents = Get-ChildItem -Path $Item
+	    $Subdir = Join-Path $Dir $Item.Name
+	    $Contents | ForEach-Object {worker $PSITEM $Subdir}
+	    return
+	}
+    }
+
+    $contents | ForEach-Object {worker $PSITEM $Target}
 }
