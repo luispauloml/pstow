b09c62b758e0c08a40078396270c0af6245b9b45
luispauloml <luispauloml@gmail.com>
2022-04-19 16:58:33 +0900

    Check if package is empty

--- winstow.psm1
+++ winstow.psm1
@@ -6,8 +6,18 @@ function winstow {
 	$Name
     )
 
+    if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true) {
+	$VerbosePreference = "Continue"
+    }
+
     $pkg = Get-ChildItem -Filter $Name -Directory
     if (!$pkg) {
 	Write-Error "$Name not found" -Category ObjectNotFound
+    }
+
+    $contents = Get-ChildItem -Path $pkg
+    if (!$contents) {
+	Write-Verbose "$Name is empty"
+	return
     }
 }
