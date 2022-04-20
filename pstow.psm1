3fa4dadd6221d8ea62fda44603a269189df6b610
luispauloml <luispauloml@gmail.com>
2022-04-20 15:06:41 +0900

    Add docstring

--- pstow.psm1
+++ pstow.psm1
@@ -1,3 +1,59 @@
+<#
+.SYNOPSIS
+Setup symbolic links for files.
+
+.DESCRIPTION
+Creates symbolics for files and directories in a fashion similar to GNU Stow.
+
+It traverses the content in the directory `PkgName` and create links
+in `Destination` as necessary.  If it finds a directory inside
+`PkgName` that does not exists in `Destination`, it creates a link for
+it in `Destination` instead of creating a folder and recursively
+creating links for its child-items.
+
+.PARAMETER PkgName
+The name of a directory inside current working directory whose
+contents will serve as a target for the links to be created.
+
+.PARAMETER Destination
+A path to the directory in which the links will be created.
+
+.PARAMETER Force
+If given, existing files -- not directories -- will be overwritten by
+a symbolic link.
+
+If given, existing references for directories -- junction, hard link
+or symbolic link to a directory -- will be overwritten by a new
+symbolic link.
+
+If not given and a file or reference to directory already exists at
+destination, an error will be written to the error stream for that
+item but execution will not be terminated.
+
+.PARAMETER WhatIf
+Does not execute any change, only shows what would happen.
+
+.PARAMETER Confirm
+Asks for confirmation before creating a symbolic link.
+
+.NOTES
+- Set-PStow does not create hard links, nor junctions, only symbolic
+  links.
+
+- Passing -Force will not override -Confirm.
+
+.EXAMPLE
+Set-PStow emacs $env:APPDATA -Verbose -WhatIf -Force
+VERBOSE: found directory 'emacs'
+VERBOSE: found directory C:\Users\<user>\AppData\Roaming
+VERBOSE: '.emacs.d' is a directory and already exists at destination.
+VERBOSE: 'lisp' is a directory and already exists at destination.
+VERBOSE: 'lisp' is already a reference to another location.
+WARNING: 'lisp' will be overwritten by a symbolic link.
+What if: Performing the operation "Create Symbolic Link" on target "Destination: C:\Users\<user>\AppData\Roaming\.emacs.d\lisp".
+WARNING: 'init.el' already exists and will be overwritten with a symbolic link.
+What if: Performing the operation "Create Symbolic Link" on target "Destination: C:\Users\<user>\AppData\Roaming\.emacs.d\init.el".
+#>
 function Set-PStow {
     [Cmdletbinding(SupportsShouldProcess=$true)]
     Param(
